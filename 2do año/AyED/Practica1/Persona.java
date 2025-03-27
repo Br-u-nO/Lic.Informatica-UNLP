f864b6bfb73f9ac40881b1b0f76774b433d17f07
Br-u-nO <brubauly@gmail.com>
2025-03-26 23:20:55 -0300

    Create Persona.java

---
+++ 2do año/AyED/Practica1/Persona.java
@@ -0,0 +1,40 @@
+package Practica1;
+
+public abstract class Persona {
+
+	private String nombre;
+	private String apellido;
+	private String email;
+	
+	
+	public Persona(String nombre, String apellido, String email) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.email = email;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	private void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	private void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getEmail() {
+		return email;
+	}
+	private void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public String toString() {
+		return " nombre: " + this.getNombre() + ", apellido: " + this.getApellido() + ", email: " + this.getEmail() ;
+	}
+	
+	
+}
