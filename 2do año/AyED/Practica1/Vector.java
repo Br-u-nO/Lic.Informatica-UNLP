b6a3509f579a4a5828a31b5546d24d7a2289c431
Br-u-nO <brubauly@gmail.com>
2025-04-02 16:53:54 -0300

    Update Vector.java

--- 2do año/AyED/Practica1/Vector.java
+++ 2do año/AyED/Practica1/Vector.java
@@ -4,15 +4,30 @@ public class Vector {
 
 	private int [] vec;
 	int diml;
-
-	public Vector(int n) {
-		this.vec = new int [n];
+    
+	public Vector (int n) {
+		int [] v=new int[n];
+		this.setVec(v);
+	}
+	public void VecMultiplos(int n) {
+		int [] v= new int [n];
+		this.setVec(v);
 		this.diml=n;
 		for (int i=0;i<n;i++) {
 			this.vec[i]=(i+1)*n;
 		}
 	}
 	
+	public int[] getVec() {
+		return vec;
+	}
+	private void setVec(int[] vec) {
+		this.vec = vec;
+	}
+	private int getDiml() {
+		return diml;
+	}
+
 	public int getNum (int n) {
 		return this.vec[n];
 	}
@@ -22,6 +37,53 @@ public class Vector {
 			aux+=(this.getNum(i)+" ");
 		}
 		return aux;
+	}
+	
+	public Valores getValores () {
+		Valores v;
+		int min=9999,max=-9999;
+		double p=0;
+		for (int i=0;i<this.getDiml();i++) {
+			if (this.getNum(i)<min) {
+				min=this.getNum(i);
+			}
+			if (this.getNum(i)>max) {
+				max=this.getNum(i);
+			}
+			p+=this.getNum(i);
+		}
+		v=new Valores(min,max,p/this.getDiml());
+		return v;
+	}
+	
+	
+
+
+	private void setVec(int n,int v) {
+		this.vec[n] = v;
+	}
+	
+
+
+	public Vector Valores2() {
+		Vector v;
+		int min=9999,max=-9999;
+		double p=0;
+		for (int i=0;i<this.getDiml();i++) {
+			if (this.getNum(i)<min) {
+				min=this.getNum(i);
+			}
+			if (this.getNum(i)>max) {
+				max=this.getNum(i);
+			}
+			p+=this.getNum(i);
+		}
+		int i=(int) (p/this.getDiml());
+		v=new Vector(3);
+		v.setVec(0, min);
+		v.setVec(1, max);
+		v.setVec(2, i);
+		return v;
 	}
 	
 }
