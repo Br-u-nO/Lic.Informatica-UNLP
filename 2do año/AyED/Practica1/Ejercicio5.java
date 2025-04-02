fb4e7a051e71eeee47cd6217f7141f5d4cabcd91
Br-u-nO <brubauly@gmail.com>
2025-04-02 19:48:42 -0300

    Update Ejercicio5.java

--- 2do año/AyED/Practica1/Ejercicio5.java
+++ 2do año/AyED/Practica1/Ejercicio5.java
@@ -3,15 +3,15 @@ package Practica1;
 public class Ejercicio5 {
 
 	public static void main(String [] args) {
-		
-		Vector v,v2;
+		int [] v2;
+		Vector v;
 		v=new Vector(0);
 		v.VecMultiplos(3);
-		Valores test=v.getValores();;
+		Valores test=v.getValores();
 		v2=v.Valores2();
 		System.out.println(v.toString());
 		System.out.println(test.toString());
-		System.out.println(v2.toString());
-		
+		System.out.println("minimo: "+v2[0]+" maximo: "+v2[1]+" promedio: "+v2[2]);
+		v.Valores3();
 	}
 }
