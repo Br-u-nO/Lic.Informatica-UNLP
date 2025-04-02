e910fa789ca70f93ecb8278a3dcb350c6326e83b
Br-u-nO <brubauly@gmail.com>
2025-04-02 16:56:53 -0300

    Update Ejercicio2.java

--- 2do año/AyED/Practica1/Ejercicio2.java
+++ 2do año/AyED/Practica1/Ejercicio2.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class Ejercicio2 {
   public static void main(String[]args) {
 	  int n;
-	  Vector vec;
+	  Vector vec=new Vector(0);
 	  try (Scanner s = new Scanner(System.in)) {
 		n=s.nextInt();
 	  }
-	  vec=new Vector(n);
+	  vec.VecMultiplos(n);;
 	  System.out.print(vec.toString());
   }
   
