1df9e1a0156e102ecbe0b3a7f33f3f24dd0dce79
Bruno Bauly <brubauly@gmail.com>
2025-04-03 20:44:41 -0300

    fe

--- 2do año/AyED/Practica1/Ejercicio7.java
+++ 2do año/AyED/Practica1/Ejercicio7.java
@@ -7,35 +7,78 @@ public class Ejercicio7 {
 
     public static void main(String[] args) {
         int n;
-        ArrayList<Integer> lista = new ArrayList<>();
+        List<Integer> lista = new ArrayList<>();
         for (int i=0;i<=5;i++){
             lista.add((i+1)*3);
         }
+        
         for (int i=0;i<=5;i++){
-            System.out.println(lista.get(i));
+            System.out.print(lista.get(i)+ " ");
         }
-         System.out.println(lista.toString());
+        System.out.println();
+        
+        
+        System.out.println(lista.toString());
+        
+        
         Iterator<Integer> iterador = lista.iterator();
         while (iterador.hasNext()) {
-        System.out.println(iterador.next());
+        System.out.print(iterador.next()+" ");
         }
-        for(Integer i:lista){
-            System.out.println(i+" ");
-        }
+        System.out.println();
+        
+        
+        lista.forEach((i) -> {
+            System.out.print(i+" ");
+        });
+        System.out.println();
+        
         
         System.out.println("_______________________________");
         
         
-        LinkedList<Integer> lista2=new LinkedList<>();
+        List<Integer> lista2=new LinkedList<>();
         for (int i=0;i<=5;i++){
             lista2.add((i+1)*5);
         }
         for (int i=0;i<=5;i++){
-            System.out.println(lista2.get(i));
+            System.out.print(lista2.get(i)+" ");
         }
-       
+        System.out.println();
+        
+        
         System.out.println("_______________________________");
         
+        List<Estudiante> estudiantes=new ArrayList<>();
+        List<Estudiante> estudiantesCopia=new LinkedList<>();
+        
+        estudiantes.add(new Estudiante("Bruno","Bauly","noimporta@gmail.com",4,4323));
+        estudiantes.add(new Estudiante("Gonzalo","Mollo","noimporta@gmail.com",7,3214));
+        estudiantes.add(new Estudiante("Romina","Tittarelli","noimporta@gmail.com",4,2341));
+        
+        estudiantesCopia=estudiantes;
+      
+        System.out.println("Primer lista");
+        imprimir(estudiantes,0);
+        System.out.println("Segunda lista");
+        imprimir(estudiantesCopia,0);
+        
+        Estudiante e=estudiantes.get(0);
+        e=new Estudiante(e.getNombre(),"Gonzales",e.getEmail(),e.getComision(),e.getDireccion());
+        estudiantes.remove(0);
+        estudiantes.add(0,e);
+        
+        System.out.println("Primer lista cambiada");
+        imprimir(estudiantes,0);
+        System.out.println("Segunda lista");
+        imprimir(estudiantesCopia,0);
     }
     
+    public static void imprimir(List<Estudiante>lista,int index){
+        if (index<lista.size()){
+            Estudiante e=lista.get(index);
+            System.out.println(e.toString());
+            imprimir(lista,index+1);
+        }
+    }
 }
