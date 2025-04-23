2723b21a2b42589a94fcc1795e9619dd307d928f
Bruno Bauly <baulyb@gmail.com>
2025-04-22 21:05:04 -0300

    Ejercicio2 arranque

--- 2do año/AyED/Practica3/RecorridosAG.java
+++ 2do año/AyED/Practica3/RecorridosAG.java
@@ -1,7 +1,26 @@
-package Practica3;
 
-public class RecorridosAG {
+
+import java.util.List;
+
+	public class RecorridosAG {
+	
+	
 	public List<Integer> numerosImparesMayoresQuePreOrden (GeneralTree<Integer> a,Integer n){
 		
+		return null;
 	}
+	public List<Integer> numerosImparesMayoresQueInOrder (GeneralTree<Integer> a,Integer n){
+		
+		return null;
+	}
+	public List<Integer>numerosImparesMayoresQuePostOrder (GeneralTree<Integer> a,Integer n){
+		
+		return null;
+	}
+	public List<Integer> numerosImparesMayoresQuePorNiveles (GeneralTree<Integer> a,Integer n){
+		
+		return null;
+	}
+	
+	
 }
